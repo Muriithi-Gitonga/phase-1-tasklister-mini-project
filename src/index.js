e6e426b6fbaec1cbbbe64ec2c155dd7a94a88403
Muriithi-Gitonga <rinkanyadavid8@gmail.com>
2022-08-24 18:27:32 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let form = document.querySelector('form')
+  form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    buildToDo(e.target.newtaskdescription.value)
+    form.reset()
+  })
+
+
 });
+
+function buildToDo(toDO) {
+  let toDOList = document.createElement('li') 
+  toDOList.textContent = `${toDO} `
+  let btn = document.createElement("button")
+  btn.addEventListener('click', deleteDiv)
+  btn.textContent = 'x'
+  toDOList.appendChild(btn)
+
+  document.querySelector("#tasks").append(toDOList)
+
+}
+
+function deleteDiv (e) {
+  e.target.parentNode.remove()
+}
+
+
